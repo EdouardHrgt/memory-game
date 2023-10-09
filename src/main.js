fd7dfc5269eb1d91bd9152e9df513bca5f45eebf
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2023-10-09 19:55:53 +0200

    Starting page form layout + responsive

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import Start from './views/StartView.vue';
+import Play from './views/GameView.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', name: 'Start', component: Start },
+    { path: '/play', name: 'Play', component: Play },
+  ],
+});
+
+createApp(App).use(router).mount('#app');
