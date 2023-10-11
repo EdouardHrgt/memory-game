9431c06b6e98437abb77a3afd37b7bd88ee4c3e8
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2023-10-12 01:05:17 +0200

    Debug images for animals theme, added speed settings, mute option

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,19 +1,28 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
+import { generateNumberGrid, generateAnimalGrid, generateGrid } from '../composables/gridGeneration';
 import clicSound from '../assets/audio/click.mp3';
 import WinSound from '../assets/audio/win.wav';
 import LooseSound from '../assets/audio/loose.mp3';
-import { generateNumberGrid, generateAnimalGrid, generateGrid } from '../composables/gridGeneration';
 
 const route = useRoute();
 const router = useRouter();
 const settings = JSON.parse(route.query.params);
-
+const isMuted = ref(false);
 const grid = ref([]);
+const speed = ref(1000);
+
 function playSound(music) {
+  if (isMuted.value === true) return;
   const audio = new Audio(music);
   audio.play();
+}
+
+function toggleSound() {
+  isMuted.value = !isMuted.value;
+  localStorage.setItem('SoundPreference', isMuted.value);
+  console.log(localStorage.getItem('SoundPreference'));
 }
 
 function backHome() {
@@ -38,6 +47,7 @@ const generateAnimals = generateAnimalGrid();
 onMounted(() => {
   // Générez la grille en fonction de settings
   grid.value = createGrid;
+  const soundPreference = localStorage.getItem('SoundPreference');
 });
 
 function reset() {
@@ -62,6 +72,19 @@ function hardReset() {
   clickedElements.value = [];
 }
 
+function addValidate(val) {
+  const buttons = document.querySelectorAll('.grid-btn');
+  buttons.forEach((btn) => {
+    if (btn.value === val) {
+      btn.classList.remove('hidden');
+      btn.classList.add('validate');
+    }
+  });
+}
+
+function toggleVictory() {
+  if (winningPairs.value.length === grid.value.length / 2) isWin.value = true;
+}
 // Save clicked elements (max 2 per round)
 const clickedElements = ref([]);
 //Cancel the ability to clic on more than 2 button per rounds
@@ -83,15 +106,10 @@ function game(event, i) {
 
   event.target.classList.remove('hidden');
 
-  let value;
-
-  if (settings.theme === 'animals') {
-    value = event.target.alt;
-  } else {
-    value = event.target.value;
-  }
+  let value = event.target.value;
 
   clickedElements.value.push({ value, index: i });
+
   const round = clickedElements.value.length === 2;
 
   // If player round completed, we block clicking event, Wait 1sec and hide buttons content again then allow a new round
@@ -106,26 +124,9 @@ function game(event, i) {
         playSound(WinSound);
         winningPairs.value.push(element1.value);
 
-        if (settings.theme === 'animals') {
-          const images = document.querySelectorAll('.img-btn');
-          images.forEach((img) => {
-            if (img.alt === element1.value) {
-              img.classList.remove('hidden');
-              img.classList.add('validate');
-            }
-          });
-        }
-        const buttons = document.querySelectorAll('.grid-btn');
-        buttons.forEach((btn) => {
-          if (btn.value === element1.value) {
-            btn.classList.remove('hidden');
-            btn.classList.add('validate');
-          }
-        });
+        addValidate(element1.value);
         // Check if the game is over
-        if (winningPairs.value.length === grid.value.length / 2) {
-          isWin.value = true;
-        }
+        toggleVictory();
         reset();
       } else {
         playSound(LooseSound);
@@ -134,24 +135,41 @@ function game(event, i) {
       }
 
       isClickable.value = true;
-    }, 950);
+    }, speed.value);
   }
 }
 </script>
 
 <template>
   <main>
-    <header class="commands flex-all">
-      <svg width="150" height="28" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M6.08 21V10.64c0-1.733.287-3.053.86-3.96s1.487-1.36 2.74-1.36c.96 0 1.647.233 2.06.7.413.467.667 1.087.76 1.86.093.773.14 1.627.14 2.56V21h6.12V10.52c0-1.733.28-3.033.84-3.9.56-.867 1.453-1.3 2.68-1.3.987 0 1.693.233 2.12.7.427.467.693 1.087.8 1.86.107.773.16 1.627.16 2.56V21h6.08V8.92c0-2.693-.48-4.753-1.44-6.18C29.04 1.313 27.293.6 24.76.6c-1.387 0-2.673.28-3.86.84a7.44 7.44 0 0 0-2.94 2.48c-.453-1.093-1.153-1.92-2.1-2.48C14.913.88 13.653.6 12.08.6c-1.467 0-2.72.347-3.76 1.04-1.04.693-1.787 1.413-2.24 2.16l-.76-2.64H0V21h6.08Zm39.2.48c1.627 0 3.22-.36 4.78-1.08 1.56-.72 2.86-1.84 3.9-3.36l-4.84-1.76c-.373.427-.9.78-1.58 1.06s-1.487.42-2.42.42c-1.12 0-2.14-.347-3.06-1.04-.92-.693-1.433-1.733-1.54-3.12H55.2c.133-2.347-.2-4.42-1-6.22-.8-1.8-1.98-3.213-3.54-4.24C49.1 1.113 47.227.6 45.04.6c-1.92 0-3.687.427-5.3 1.28a10.074 10.074 0 0 0-3.88 3.6c-.973 1.547-1.46 3.373-1.46 5.48 0 2.187.46 4.067 1.38 5.64a9.228 9.228 0 0 0 3.84 3.62c1.64.84 3.527 1.26 5.66 1.26ZM49.56 9h-9.04c.187-1.36.727-2.347 1.62-2.96.893-.613 1.913-.92 3.06-.92 1.173 0 2.187.327 3.04.98.853.653 1.293 1.62 1.32 2.9Zm14.6 12V10.64c0-1.733.287-3.053.86-3.96s1.487-1.36 2.74-1.36c.96 0 1.647.233 2.06.7.413.467.667 1.087.76 1.86.093.773.14 1.627.14 2.56V21h6.12V10.52c0-1.733.28-3.033.84-3.9.56-.867 1.453-1.3 2.68-1.3.987 0 1.693.233 2.12.7.427.467.693 1.087.8 1.86.107.773.16 1.627.16 2.56V21h6.08V8.92c0-2.693-.48-4.753-1.44-6.18C87.12 1.313 85.373.6 82.84.6c-1.387 0-2.673.28-3.86.84a7.44 7.44 0 0 0-2.94 2.48c-.453-1.093-1.153-1.92-2.1-2.48C72.993.88 71.733.6 70.16.6c-1.467 0-2.72.347-3.76 1.04-1.04.693-1.787 1.413-2.24 2.16l-.76-2.64h-5.32V21h6.08Zm38.84.48c1.813 0 3.52-.373 5.12-1.12 1.6-.747 2.893-1.9 3.88-3.46.987-1.56 1.48-3.527 1.48-5.9 0-2.4-.493-4.367-1.48-5.9-.987-1.533-2.28-2.667-3.88-3.4-1.6-.733-3.307-1.1-5.12-1.1-1.84 0-3.56.367-5.16 1.1-1.6.733-2.893 1.867-3.88 3.4-.987 1.533-1.48 3.5-1.48 5.9 0 2.373.493 4.34 1.48 5.9.987 1.56 2.28 2.713 3.88 3.46 1.6.747 3.32 1.12 5.16 1.12Zm0-4.72c-1.227 0-2.253-.44-3.08-1.32-.827-.88-1.24-2.36-1.24-4.44 0-2.107.42-3.58 1.26-4.42.84-.84 1.86-1.26 3.06-1.26 1.173 0 2.18.42 3.02 1.26.84.84 1.26 2.313 1.26 4.42 0 2.08-.407 3.56-1.22 4.44-.813.88-1.833 1.32-3.06 1.32ZM122.52 21V11c0-1.573.393-2.707 1.18-3.4.787-.693 1.913-1.04 3.38-1.04.293 0 .56.007.8.02.24.013.52.047.84.1V.6c-1.52-.08-2.813.2-3.88.84-1.067.64-1.907 1.76-2.52 3.36l-1.08-3.64h-4.8V21h6.08Zm11.36 6.44c1.733 0 3.107-.187 4.12-.56 1.013-.373 1.847-1.027 2.5-1.96s1.3-2.24 1.94-3.92l7.6-19.84h-6.52l-3.8 12.4-3.88-12.4h-6.52l7.6 19.72c-.213.667-.447 1.153-.7 1.46-.253.307-.607.5-1.06.58-.453.08-1.133.12-2.04.12h-1.2v4.4h1.96Z"
-          fill="rgb(252, 252, 252)"
-          fill-rule="nonzero"
-        />
-      </svg>
-      <div class="btn-wrapper flex">
-        <button class="restart" @click="restart()">Restart</button>
-        <button class="new" @click="backHome()">New Game</button>
+    <header class="commands flex">
+      <div class="logo">
+        <svg width="150" height="28" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M6.08 21V10.64c0-1.733.287-3.053.86-3.96s1.487-1.36 2.74-1.36c.96 0 1.647.233 2.06.7.413.467.667 1.087.76 1.86.093.773.14 1.627.14 2.56V21h6.12V10.52c0-1.733.28-3.033.84-3.9.56-.867 1.453-1.3 2.68-1.3.987 0 1.693.233 2.12.7.427.467.693 1.087.8 1.86.107.773.16 1.627.16 2.56V21h6.08V8.92c0-2.693-.48-4.753-1.44-6.18C29.04 1.313 27.293.6 24.76.6c-1.387 0-2.673.28-3.86.84a7.44 7.44 0 0 0-2.94 2.48c-.453-1.093-1.153-1.92-2.1-2.48C14.913.88 13.653.6 12.08.6c-1.467 0-2.72.347-3.76 1.04-1.04.693-1.787 1.413-2.24 2.16l-.76-2.64H0V21h6.08Zm39.2.48c1.627 0 3.22-.36 4.78-1.08 1.56-.72 2.86-1.84 3.9-3.36l-4.84-1.76c-.373.427-.9.78-1.58 1.06s-1.487.42-2.42.42c-1.12 0-2.14-.347-3.06-1.04-.92-.693-1.433-1.733-1.54-3.12H55.2c.133-2.347-.2-4.42-1-6.22-.8-1.8-1.98-3.213-3.54-4.24C49.1 1.113 47.227.6 45.04.6c-1.92 0-3.687.427-5.3 1.28a10.074 10.074 0 0 0-3.88 3.6c-.973 1.547-1.46 3.373-1.46 5.48 0 2.187.46 4.067 1.38 5.64a9.228 9.228 0 0 0 3.84 3.62c1.64.84 3.527 1.26 5.66 1.26ZM49.56 9h-9.04c.187-1.36.727-2.347 1.62-2.96.893-.613 1.913-.92 3.06-.92 1.173 0 2.187.327 3.04.98.853.653 1.293 1.62 1.32 2.9Zm14.6 12V10.64c0-1.733.287-3.053.86-3.96s1.487-1.36 2.74-1.36c.96 0 1.647.233 2.06.7.413.467.667 1.087.76 1.86.093.773.14 1.627.14 2.56V21h6.12V10.52c0-1.733.28-3.033.84-3.9.56-.867 1.453-1.3 2.68-1.3.987 0 1.693.233 2.12.7.427.467.693 1.087.8 1.86.107.773.16 1.627.16 2.56V21h6.08V8.92c0-2.693-.48-4.753-1.44-6.18C87.12 1.313 85.373.6 82.84.6c-1.387 0-2.673.28-3.86.84a7.44 7.44 0 0 0-2.94 2.48c-.453-1.093-1.153-1.92-2.1-2.48C72.993.88 71.733.6 70.16.6c-1.467 0-2.72.347-3.76 1.04-1.04.693-1.787 1.413-2.24 2.16l-.76-2.64h-5.32V21h6.08Zm38.84.48c1.813 0 3.52-.373 5.12-1.12 1.6-.747 2.893-1.9 3.88-3.46.987-1.56 1.48-3.527 1.48-5.9 0-2.4-.493-4.367-1.48-5.9-.987-1.533-2.28-2.667-3.88-3.4-1.6-.733-3.307-1.1-5.12-1.1-1.84 0-3.56.367-5.16 1.1-1.6.733-2.893 1.867-3.88 3.4-.987 1.533-1.48 3.5-1.48 5.9 0 2.373.493 4.34 1.48 5.9.987 1.56 2.28 2.713 3.88 3.46 1.6.747 3.32 1.12 5.16 1.12Zm0-4.72c-1.227 0-2.253-.44-3.08-1.32-.827-.88-1.24-2.36-1.24-4.44 0-2.107.42-3.58 1.26-4.42.84-.84 1.86-1.26 3.06-1.26 1.173 0 2.18.42 3.02 1.26.84.84 1.26 2.313 1.26 4.42 0 2.08-.407 3.56-1.22 4.44-.813.88-1.833 1.32-3.06 1.32ZM122.52 21V11c0-1.573.393-2.707 1.18-3.4.787-.693 1.913-1.04 3.38-1.04.293 0 .56.007.8.02.24.013.52.047.84.1V.6c-1.52-.08-2.813.2-3.88.84-1.067.64-1.907 1.76-2.52 3.36l-1.08-3.64h-4.8V21h6.08Zm11.36 6.44c1.733 0 3.107-.187 4.12-.56 1.013-.373 1.847-1.027 2.5-1.96s1.3-2.24 1.94-3.92l7.6-19.84h-6.52l-3.8 12.4-3.88-12.4h-6.52l7.6 19.72c-.213.667-.447 1.153-.7 1.46-.253.307-.607.5-1.06.58-.453.08-1.133.12-2.04.12h-1.2v4.4h1.96Z"
+            fill="rgb(252, 252, 252)"
+            fill-rule="nonzero"
+          />
+        </svg>
+        <div class="speed">
+          <input type="range" min="500" max="2000" name="speed" id="speed" v-model="speed" />
+          <label for="speed"
+            >round speed: <span>{{ speed }}ms</span></label
+          >
+        </div>
+      </div>
+
+      <div class="header-icons-btns flex">
+        <div class="btn-wrapper flex">
+          <button class="restart" @click="restart()">Restart</button>
+          <button class="new" @click="backHome()">New Game</button>
+        </div>
+        <button v-if="isMuted === false" class="sound-icon" @click="toggleSound()">
+          <img src="../assets/sound.png" alt="Sound On" />
+        </button>
+        <button v-if="isMuted === true" class="sound-icon" @click="toggleSound()">
+          <img src="../assets/no-sound.png" alt="Sound Off" />
+        </button>
       </div>
     </header>
     <section>
@@ -163,27 +181,23 @@ function game(event, i) {
         <button class="grid-btn hidden" v-for="(el, i) in grid" :key="el" :value="el" @click="game($event, i)">
           {{ el }}
         </button>
-        <div class="win-modal" v-if="isWin">
-          <h1>GG <br />You win!</h1>
-        </div>
+        <div class="win-modal" v-if="isWin"></div>
       </div>
       <div
         class="grid"
         :class="{ 'large-grid': settings.grid == 1, 'small-grid': settings.grid == 0 }"
         v-if="settings.theme === 'animals'"
       >
-        <img
-          class="img-btn hidden"
+        <button
+          class="grid-btn hidden grid-btn-animals"
           v-for="(el, i) in grid"
           :key="el.id"
           :value="el.value"
           @click="game($event, i)"
-          :src="getImageUrl(el.img)"
-          :alt="el.value"
-        />
-        <div class="win-modal" v-if="isWin">
-          <h1>GG <br />You win!</h1>
-        </div>
+          :style="{ backgroundImage: `url(${getImageUrl(el.img)})` }"
+        ></button>
+
+        <div class="win-modal" v-if="isWin"></div>
       </div>
     </section>
     <footer>
@@ -199,17 +213,46 @@ function game(event, i) {
 main {
   width: 100%;
   min-height: 100dvh;
-  --max-w: 1024px;
+  --max-w: 950px;
   background-color: var(--clr-dark-gray);
 }
+
 header {
   max-width: var(--max-w);
   margin-inline: auto;
   padding: 6rem 0 4rem;
+  align-items: baseline;
+  justify-content: flex-end;
+  gap: 1rem;
+}
+
+.logo {
+  margin-right: auto;
+}
+
+.speed {
+  margin-top: 0.5rem;
+}
+
+.speed label {
+  display: block;
+  color: var(--clr-orange);
+  font-size: 22px;
+}
+
+label span {
+  font-size: 18px;
+  letter-spacing: 0.5px;
+  color: var(--clr-light-gray);
+}
+
+.header-icons-btns {
+  align-items: center;
 }
 
 .btn-wrapper {
   gap: 1rem;
+  margin-right: 2rem;
 }
 
 .btn-wrapper button {
@@ -217,6 +260,19 @@ header {
   border-radius: 6rem;
   font-size: 20px;
   font-weight: bold;
+  max-height: 3rem;
+}
+
+.sound-icon {
+  padding: 0;
+  background: transparent;
+  width: 4.2rem;
+  aspect-ratio: 1;
+  transition: 0.3s ease-in-out;
+}
+
+.sound-icon:hover {
+  filter: grayscale(1);
 }
 
 .restart {
@@ -224,16 +280,27 @@ header {
   color: var(--clr-snow-white);
 }
 
+.restart:hover {
+  background-color: var(--clr-snow-white);
+  color: var(--clr-orange);
+}
+
 .new {
   background-color: var(--clr-snow-white);
   color: var(--clr-dark-gray);
 }
 
+.new:hover {
+  background-color: var(--clr-light-blue);
+  color: var(--clr-snow-white);
+}
+
 .win-modal {
-  background-color: var(--clr-snow-white);
+  background-color: var(--clr-dark-gray);
+  background-image: url('../assets/winer.png');
+  background-position: center;
   position: absolute;
   inset: 0;
-  z-index: 100;
   display: grid;
   place-content: center;
   border-radius: 10px;
@@ -281,6 +348,8 @@ header {
   align-items: center;
   justify-content: center;
   position: relative;
+  background-size: 72%;
+  background-position: center;
 }
 
 .grid-btn-img {
@@ -292,6 +361,7 @@ header {
   color: rgb(86, 97, 109);
   /* color: transparent; */
   animation: hide 0.4s ease-in-out forwards;
+  background-size: 0;
 }
 
 .validate {
@@ -368,6 +438,9 @@ footer p {
     padding: 3rem 3rem 1rem;
     flex-direction: column-reverse;
     gap: 3rem;
+  }
+  header svg {
+    margin: 0 auto;
   }
   .grid {
     padding: 1.5rem 0.5rem 0;
